a1e1f27e248d5b4048c0738d75786b5ca6422e65
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-04-17 12:52:02 +0530

    delete a file

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -12,7 +12,7 @@ function customRender(reactElement, container){
 
     const domElement = document.createElement
     (reactElement.type)
-    domElement.innerHTML = reactElement.children
+    domElement.innerHTML = reactElement.children    
     for (const prop in reactElement.props){
         if(prop === 'children') continue;
         domElement.setAttribute(prop, reactElement.props [prop])
