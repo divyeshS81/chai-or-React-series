0f4fbd01ab270ea5f1c41cbab6dadc8e3670fd02
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-04-18 12:58:19 +0530

    creat project with hooks

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,30 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  let [counter, setCounter] = useState(15)
+
+  const addValue = () => {
+    counter = counter + 1
+    setCounter(counter)
+  }
+
+  const removeValue = () =>{
+    setCounter(counter - 1)
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>chai aur react</h1>
+     <h2>counter value : {counter}</h2>
+     
+     <button onClick={addValue}>Add value {counter} </button><br/>
+     <button onClick={removeValue}>Remove value {counter}</button>
+     <p>footer {counter}</p>
     </>
   )
 }
