a453e81eccedfb61a13ee469865f2d59b96b0e3c
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-04-19 12:43:35 +0530

    create cards with tailwind css with use of props (property)

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,32 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './assets/components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj ={
+    username : "Divyesh",
+    age: 21
+  }
+  let newArray = [1,2,3]
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username = "chai aur react" btnText = "click me"/>
+      <Card username = "Divyesh" btnText = "visit me"/>
     </>
   )
 }
