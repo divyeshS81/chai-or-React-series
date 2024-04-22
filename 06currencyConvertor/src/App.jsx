fc464f7a9644817a19967ecbe9ef793b22405309
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-04-22 12:56:52 +0530

    create a new project the project name is currency convertor

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1 className='text-3xl bg-orange-500'>Currency App With React </h1>
+    </>
+  )
+}
+
+export default App
