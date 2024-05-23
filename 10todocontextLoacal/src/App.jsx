e9975659d404a580ea29231310311fd582860047
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-05-23 12:05:44 +0530

    add todo list project ion this commit

---
+++ 10todocontextLoacal/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import { TodoProvider } from './context'
+
+function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now(), ...todo},...prev])
+  }
+
+  return (
+    <TodoProvider value={{todos, addTodo, updatedTodo, deleteTodo, toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+    </TodoProvider>
+  )
+}
+
+export default App
